7be3ff7a64552502d83f476674ae65d9e0f06ab8
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-08-29 18:39:15 +0530

    first commit

---
+++ Ass13/Controllers/TeamController.cs
@@ -0,0 +1,25 @@
+﻿using Ass13.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ass13.Controllers
+{
+    public class PlayerController : Controller
+    {
+        List<Team> listTeam = new List<Team>()
+        {
+            new Team(){TeamId = 1,TeamName="India",NOWC=2},
+            new Team(){TeamId = 2,TeamName="Australia",NOWC=4},
+            new Team(){TeamId = 3,TeamName="West Indies",NOWC=2},
+            new Team(){TeamId = 4,TeamName="England",NOWC=1}
+        };
+        // GET: Player
+        public ActionResult Index()
+        {
+            return View(listTeam);
+        }
+    }
+}
